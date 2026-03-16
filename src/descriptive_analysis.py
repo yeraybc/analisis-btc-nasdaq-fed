3010264a953aecfb1c59dd27e59c5ed052817a8c
yeraybc <yera0304@gmail.com>
2026-03-16 14:01:06 +0100

    docs: cambio 2 lineas de comentario de codigo

--- src/descriptive_analysis.py
+++ src/descriptive_analysis.py
@@ -16,8 +16,10 @@ def get_descriptive_stats(df):
         pd.DataFrame: Tabla de estadísticos descriptivos transpuesta y redondeada 
             a dos decimales para mejorar la legibilidad en el portfolio.
     """
-    # Excluimos columnas no numéricas si las hubiera para el cálculo
+    # Excluimos columnas no numéricas si las hubiera para el cálculo. Selección de métricas estándar de dispersión y tendencia
     stats = df.describe().T[['mean', 'std', 'min', '50%', 'max']]
+
+    # Incorporación de métricas de forma (cruciales para el análisis de riesgo)
     stats['skew'] = df.skew() 
     stats['kurtosis'] = df.kurtosis()
     return stats.round(2)
